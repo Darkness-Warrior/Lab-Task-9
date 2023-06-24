97041590342cf8b7033f0b5838f3c7758b8e41b6
Egor <123231130+Darkness-Warrior@users.noreply.github.com>
2023-06-24 04:22:35 +0300

    Update Task_9(V.0).py

--- Task_9(V.0).py
+++ Task_9(V.0).py
@@ -1,41 +1,67 @@
-class CustomException1(BaseException):
+class Cla(BaseException):
     pass
-class CustomException2(BaseException):
+
+
+class Clas(BaseException):
     pass
-class CustomException3(BaseException):
+
+
+class Class(BaseException):
     pass
+
+
 class MyClass:
     def __init__(self, value):
         self.value = value
+
     def method1(self):
-        if self.value < 0:
-            raise CustomException1("Value should be greater than 0")
+        if self.value <= 0:
+            raise Cla("Значение должно быть больше 0")
         return self.value
+
     def method2(self):
         if self.value > 100:
-            raise CustomException2("Value should be less than or equal to 100")
+            raise Clas("Значение должно быть меньше или равно 100")
         return self.value * 2
+
     def method3(self):
         if self.value % 2 != 0:
-            raise CustomException3("Value should be even")
+            raise Class("Значение должно быть четным")
         return self.value / 2
+
+
 class ExceptionHandler:
     def __init__(self, obj):
         self.obj = obj
+
     def handle_exceptions(self):
         try:
             print(self.obj.method1())
             print(self.obj.method2())
             print(self.obj.method3())
-        except CustomException1 as e:
+        except Cla as e:
             print(f"Error: {e}")
-        except CustomException2 as e:
+        except Clas as e:
             print(f"Error: {e}")
-        except CustomException3 as e:
+        except Class as e:
             print(f"Error: {e}")
         except Exception as e:
             print("Unknown error occurred")
             print(e)
-obj = MyClass(-5)
+
+
+obj = MyClass(10)    # Правильный вывод
+handler = ExceptionHandler(obj)
+handler.handle_exceptions()
+
+obj = MyClass(0)
+handler = ExceptionHandler(obj)
+handler.handle_exceptions()
+
+obj = MyClass(150)
+handler = ExceptionHandler(obj)
+handler.handle_exceptions()
+
+obj = MyClass(3)
 handler = ExceptionHandler(obj)
 handler.handle_exceptions()
